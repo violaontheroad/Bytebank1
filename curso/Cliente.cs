6b465e14b5d9a423aa8fb95170f1b2d5337b146e
André Bessa <andrebessax@gmail.com>
2022-02-23 10:53:47 -0300

    Ajuste

--- curso/Cliente.cs
+++ curso/Cliente.cs
@@ -12,13 +12,13 @@ namespace bytebank
         {
             this.Cpf = cpf;
             this.Nome = nome;
-            this.Profissão = profissao;
+            this.Profissao = profissao;
         }    
         private string cpf;
         private string nome { get; set; }
         private string profissao { get; set; }
         public string Cpf { get => cpf; set => cpf = value; }
         public string Nome { get => nome; set => nome = value; }
-        public string Profissão { get => profissao; set => profissao = value; }
+        public string Profissao { get => profissao; set => profissao = value; }
     }
 }
